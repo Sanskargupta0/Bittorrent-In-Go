f9ba89bc7c76f9f6d8e6d09cf9cf7e6cfb00233b
Sanskar gupta <Sanskar362002@gmail.com>
2025-07-25 11:05:00 +0530

    Decode bencoded lists

--- app/main.go
+++ app/main.go
@@ -1,63 +1,138 @@
 package main
 
 import (
+	// Uncomment this line to pass the first stage
+	// "encoding/json"
 	"encoding/json"
 	"fmt"
+	"io"
 	"os"
-	"strconv"
-	"unicode"
 	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
 
-// Ensures gofmt doesn't remove the "os" encoding/json import (feel free to remove this!)
-var _ = json.Marshal
+func decode(b string, st int) (x interface{}, i int, err error) {
+	if st == len(b) {
+		return nil, st, io.ErrUnexpectedEOF
+	}
 
-// Example:
-// - 5:hello -> hello
-// - 10:hello12345 -> hello12345
-func decodeBencode(bencodedString string) (interface{}, error) {
-	if unicode.IsDigit(rune(bencodedString[0])) {
-		var firstColonIndex int
+	i = st
 
-		for i := 0; i < len(bencodedString); i++ {
-			if bencodedString[i] == ':' {
-				firstColonIndex = i
-				break
-			}
-		}
-
-		lengthStr := bencodedString[:firstColonIndex]
-
-		length, err := strconv.Atoi(lengthStr)
-		if err != nil {
-			return "", err
-		}
-
-		return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
-	} else if rune(bencodedString[0]) == 'i' {
-		return strconv.Atoi(bencodedString[1 : len(bencodedString)-1])
-	} else {
-		return "", fmt.Errorf("Only strings are supported at the moment")
+	switch {
+	case b[i] == 'l':
+		return decodeList(b, i)
+	case b[i] == 'i':
+		return decodeInt(b, i)
+	case b[i] >= '0' && b[i] <= '9':
+		return decodeString(b, i)
+	default:
+		return nil, st, fmt.Errorf("unexpected value: %q", b[i])
 	}
 }
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
+func decodeString(b string, st int) (x string, i int, err error) {
+	var l int
 
+	i = st
+
+	for i < len(b) && b[i] >= '0' && b[i] <= '9' {
+		l = l*10 + (int(b[i]) - '0')
+		i++
+	}
+
+	if i == len(b) || b[i] != ':' {
+		return "", st, fmt.Errorf("bad string")
+	}
+
+	i++
+
+	if i+l > len(b) {
+		return "", st, fmt.Errorf("bad string: out of bounds")
+	}
+
+	x = b[i : i+l]
+	i += l
+
+	return x, i, nil
+}
+
+func decodeInt(b string, st int) (x int, i int, err error) {
+	i = st
+	i++ // 'i'
+
+	if i == len(b) {
+		return 0, st, fmt.Errorf("bad int")
+	}
+
+	neg := false
+
+	if b[i] == '-' {
+		neg = true
+		i++
+	}
+
+	for i < len(b) && b[i] >= '0' && b[i] <= '9' {
+		x = x*10 + (int(b[i]) - '0')
+		i++
+	}
+
+	if i == len(b) || b[i] != 'e' {
+		return 0, st, fmt.Errorf("bad int")
+	}
+
+	i++
+
+	if neg {
+		x = -x
+	}
+
+	return x, i, nil
+}
+
+func decodeList(b string, st int) (l []interface{}, i int, err error) {
+	i = st
+	i++ // 'l'
+
+	l = make([]interface{}, 0)
+
+	for {
+		if i >= len(b) {
+			return nil, st, fmt.Errorf("bad list")
+		}
+
+		if b[i] == 'e' {
+			break
+		}
+
+		var x interface{}
+
+		x, i, err = decode(b, i)
+		if err != nil {
+			return nil, i, err
+		}
+
+		l = append(l, x)
+	}
+
+	return l, i, nil
+}
+
+func main() {
 	command := os.Args[1]
 
 	if command == "decode" {
-		bencodedValue := os.Args[2]
-
-		decoded, err := decodeBencode(bencodedValue)
+		x, _, err := decode(os.Args[2], 0)
 		if err != nil {
-			fmt.Println(err)
-			return
+			fmt.Printf("error: %v\n", err)
+			os.Exit(1)
 		}
 
-		jsonOutput, _ := json.Marshal(decoded)
-		fmt.Println(string(jsonOutput))
+		y, err := json.Marshal(x)
+		if err != nil {
+			fmt.Printf("error: encode to json%v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("%s\n", y)
 	} else {
 		fmt.Println("Unknown command: " + command)
 		os.Exit(1)
